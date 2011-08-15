a8635ddee487118adb5ae0acc36d2934b80a61b0
Marc Qualie <marc@marcqualie.com>
2011-08-15 13:48:30 +0100

    Added Blogger test script

---
+++ php/blogger.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+ *	
+ *		Basic Blogger Data Dump
+ *		This file will download your blogger feed and save to log file
+ *
+ */
+
+include ('class.feedparser.php');
+$feed = new FeedParser();
+
+$limit		= 10;
+$user		= 'blog.marcqualie.com';
+$data		= $feed->blogger($user, $limit);
+
+$fh = fopen ('blogger.log', 'w');
+  fwrite($fh, print_r($data, 1));
+  fclose($fh);
+
+echo "Downloaded " . count($data['posts']) . " posts\n";
+echo "Bandwidth Usage: {$feed->bandwidth}\n";
+echo "Requests: {$feed->requests}\n";
+echo "\n";
